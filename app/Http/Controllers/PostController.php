ba3dd312eec750837141e5440495706a8436ef79
K1SONES <maplekaiser7@gmail.com>
2023-07-20 21:34:19 +0900

    かっこの種類を間違えていた

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -22,10 +22,10 @@ class PostController extends Controller
    }    
    // 作成機能
    public function store(Request $request) {
-      $request->validate({
+      $request->validate([
          'title' => 'required',
          'content' => 'required',
-      });
+      ]);
 
        $post = new Post();
        $post->title = $request->input('title');
@@ -44,10 +44,10 @@ class PostController extends Controller
    }
    // 更新機能
    public function update(Request $request, Post $post) {
-      $request->validate({
+      $request->validate([
          'title' => 'required',
          'content' => 'required',
-      });
+      ]);
 
       $post->title = $request->input('title');
       $post->content = $request->input('content');
