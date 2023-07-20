7715a0f8ff7872c0aeb69322c216b4e6207116a3
K1SONES <maplekaiser7@gmail.com>
2023-07-20 21:43:14 +0900

    エラー時のリセット対策

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -38,11 +38,11 @@
                     @csrf
                     <div>
                         <label for="title">タイトル</label>
-                        <input type="text" name="title">
+                        <input type="text" name="title" value="{{ old('title') }}">
                     </div>
                     <div>
                         <label for="content">本文</label>
-                        <textarea name="content"></textarea>
+                        <textarea name="content">{{ old('title') }}</textarea>
                     </div>
                     <button type="submit">投稿</button>
                 </form>
