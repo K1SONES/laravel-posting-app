ffb9cb04915b9b3c220fe1896c165e0a09714bd4
K1SONES <maplekaiser7@gmail.com>
2023-07-21 23:03:00 +0900

    間違いがあった

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -36,7 +36,7 @@ class PostController extends Controller
    }
    // 詳細ページ
    public function show(post $post) {
-      return view('posts,show',compact('post'));
+      return view('posts/show',compact('post'));
    } 
    // 更新ページ
    public function edit(Post $post) {
